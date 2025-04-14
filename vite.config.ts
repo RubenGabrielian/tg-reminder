1572f4bf96600b7ecf212042e90dc3fb8e9d4f9d
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-04-14 12:27:55 +0400

    sdsd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  }
 })
